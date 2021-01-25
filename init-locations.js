45ff1cea968ab9754d32b74c4e19e43bac52d863
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-01-25 13:01:20 -0600

    geo

--- init-locations.js
+++ init-locations.js
@@ -1,1 +1,23 @@
-
+export default [
+    {
+   Name: "Colden hall",
+   North: 40.3508200,
+   West: -94.8824554,
+   South: 40.3510490,
+   East: -94.8823754,
+   Notes: '',
+   Attachments: '',
+   'Quest-Location': 'Colden hall',
+ },
+ {
+    Name: 'Horizons west Apartments',
+    North: 40.3560533,
+    West: -94.8822882,
+    South: 40.3560296,
+    East: -94.8823180,
+    Notes: '',
+    Attachments: '',
+    'Quest-Location': 'Horizons west Apartments',
+  },
+ 
+];
