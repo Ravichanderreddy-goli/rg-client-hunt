5048bdd68eacb6c17940f7e755f55bb85434588b
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-01-29 19:39:50 -0600

    added audio

--- init-locations.js
+++ init-locations.js
@@ -1,23 +1,23 @@
 export default [
       
   {
-      Name: 'Horizons West Apartments ',
-      Latitude: 40.3589695, 
-      Longitude: -94.8831951,
-  North: 40.3589695,
+       Name: 'Horizons West Apartments ',
+       Latitude: 40.3589695, 
+       Longitude: -94.8831951,
+   North: 40.3589695,
       West: -94.8831951,
      South: 40.3589695,
        East: -94.8831951,
    },
       
   
-  {
+   {
       Name: 'Colden Pond',
       Latitude: 40.33229497033568,
-      Longitude: -94.874864169312,
-      North: 40.33229497033568, 
-      West: -94.874864169312,
-      South: 40.33229497033568, 
-      East: -94.874864169312,
+       Longitude: -94.874864169312,
+       North: 40.33229497033568, 
+       West: -94.874864169312,
+       South: 40.33229497033568, 
+       East: -94.874864169312,
   }
 ]
