296fc3b4422e27935f0a537430934941ae132ca0
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-02-01 12:56:22 -0600

    locations

--- init-locations.js
+++ init-locations.js
@@ -2,29 +2,23 @@ export default [
       
   {
        Name: 'Horizons West Apartments ',
-       Latitude: 40.3589695, 
-       Longitude: -94.8831951,
-   North: 40.3589695,
-      West: -94.8831951,
-     South: 40.3589695,
-       East: -94.8831951,
+       Latitude: 40.3559331, 
+       Longitude: -94.8819555,
+  
    },
       
   
    {
       Name: 'Colden Pond',
-      Latitude: 40.33229497033568,
-       Longitude: -94.874864169312,
-       North: 40.33229497033568, 
-       West: -94.874864169312,
-       South: 40.33229497033568, 
-       East: -94.874864169312,
+      Latitude: 40.3499213,
+      Longitude: -94.8830229,
+       
   },
   {
     Name: 'Bearcat Stadium',
-    Latitude: 40.35073819617418, 
-    Longitude: -94.88584822342064
+    Latitude: 40.3506406, 
+    Longitude: -94.8858476,
   }
-  
+
 
 ]
